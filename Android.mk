b4acd9c39c0a5423e375e18e10ce95607f15e55f
Calin Juravle <calin@google.com>
2015-06-08 14:27:39 +0100

    [WIP] Build libnativehelper as static library
    Change-Id: I82cd4d56c1d97c0016320294ae0ca802b3a6d939

--- Android.mk
+++ Android.mk
@@ -62,7 +62,6 @@ LOCAL_NDK_STL_VARIANT := c++_static
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 include $(BUILD_SHARED_LIBRARY)
 
-
 #
 # Build for the host.
 #
@@ -81,6 +80,26 @@ LOCAL_LDFLAGS := -ldl
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MULTILIB := both
 include $(BUILD_HOST_SHARED_LIBRARY)
+
+
+#
+# Build static for the host.
+#
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnativehelper
+LOCAL_MODULE_TAGS := optional
+LOCAL_CLANG := true
+LOCAL_SRC_FILES := \
+    $(local_src_files) \
+    JniInvocation.cpp
+LOCAL_CFLAGS := -Werror -fvisibility=protected
+LOCAL_C_INCLUDES := libcore/include
+LOCAL_STATIC_LIBRARIES := liblog
+LOCAL_LDFLAGS := -ldl
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+LOCAL_MULTILIB := both
+include $(BUILD_HOST_STATIC_LIBRARY)
 
 #
 # Tests.
