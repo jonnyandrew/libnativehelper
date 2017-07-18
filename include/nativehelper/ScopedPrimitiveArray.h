59ecd18255b803b12ee9ef637ebd94f44f6996e2
Steven Moreland <smoreland@google.com>
2017-07-18 16:53:53 -0700

    libnativehelper: use proper nativehelper headers
    libnativehelper exports headers under nativehelper. These were
    available before incorrectly as global headers in order to give
    access to jni.h.
    
    Test: modules using libnativehelper find headers
    Bug: 63762847
    Change-Id: Ie51ced4354f743acafff0d9aaf17d72cc477573f

--- include/nativehelper/ScopedPrimitiveArray.h
+++ include/nativehelper/ScopedPrimitiveArray.h
@@ -17,7 +17,7 @@
 #ifndef SCOPED_PRIMITIVE_ARRAY_H_included
 #define SCOPED_PRIMITIVE_ARRAY_H_included
 
-#include "JNIHelp.h"
+#include <nativehelper/JNIHelp.h>
 
 #ifdef POINTER_TYPE
 #error POINTER_TYPE is defined.
