c8102c74f5135fe27efd58f1dc1e408b05ad2d37
Alex Light <allight@google.com>
2018-10-31 14:50:54 -0700

    Enable libnativehelper with linux_bionic
    Test: build
    Test: run linux_bionic gtests
    Bug: 31559095
    Change-Id: I840f8c912f8a70c2afdaf7a1f2083e7b1f33c546

--- JNIHelp.cpp
+++ JNIHelp.cpp
@@ -361,9 +361,12 @@ void jniLogException(C_JNIEnv* env, int priority, const char* tag, jthrowable ex
 }
 
 const char* jniStrError(int errnum, char* buf, size_t buflen) {
-#if __GLIBC__
+#if __GLIBC__ || (defined(_GNU_SOURCE) && defined(__ANDROID_API__) && __ANDROID_API__ >= 23)
     // Note: glibc has a nonstandard strerror_r that returns char* rather than POSIX's int.
-    // char *strerror_r(int errnum, char *buf, size_t n);
+    // char // *strerror_r(int errnum, char *buf, size_t n); When we are not __ANDROID__ and
+    // __ANDROID_API__ >= 23 then bionic will provide a gnu-compatible implementation of strerror_r
+    // instead of the bsd version. This can happen with linux_bionic builds.
+    // TODO(b/31559095) Figure out why this is triggered by linux_bionic builds.
     return strerror_r(errnum, buf, buflen);
 #else
     int rc = strerror_r(errnum, buf, buflen);
