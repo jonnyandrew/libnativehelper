c95dc7256748d479f5733ef4837960701efca557
Jerome Gaillard <jgaillard@google.com>
2019-03-12 15:17:22 +0000

    Enable libnativehelper for Windows
    This requires using the Windows specific API for loading/manipulating
    native libraries, and string printing.
    libnativehelper is a dependency of layoutlib native (used for desktop
    rendering of Android resources), which is why it needs a Windows verion.
    
    Bug: 117921091
    Test: lunch sdk && make libnativehelper
    Change-Id: I20fa392ac7e3c031ce076435bf4380205992608b

--- JniInvocation.cpp
+++ JniInvocation.cpp
@@ -16,7 +16,11 @@
 
 #include "nativehelper/JniInvocation.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <dlfcn.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 
@@ -55,6 +59,56 @@ int GetLibrarySystemProperty(char* buffer) {
 #endif
 }
 
+#define STRINGIFY_INNER(x) #x
+#define STRINGIFY(x) STRINGIFY_INNER(x)
+
+#ifdef _WIN32
+#define FUNC_POINTER FARPROC
+#define ERROR_FORMAT_TYPE %lu
+#define ERROR_TYPE DWORD
+#else
+#define FUNC_POINTER void*
+#define ERROR_FORMAT_TYPE %s
+#define ERROR_TYPE char*
+#endif
+
+void* OpenLibrary(const char* filename) {
+#ifdef _WIN32
+  return LoadLibrary(filename);
+#else
+  // Load with RTLD_NODELETE in order to ensure that libart.so is not unmapped when it is closed.
+  // This is due to the fact that it is possible that some threads might have yet to finish
+  // exiting even after JNI_DeleteJavaVM returns, which can lead to segfaults if the library is
+  // unloaded.
+  const int kDlopenFlags = RTLD_NOW | RTLD_NODELETE;
+  return dlopen(filename, kDlopenFlags);
+#endif
+}
+
+int CloseLibrary(void* handle) {
+#ifdef _WIN32
+  return FreeLibrary(static_cast<HMODULE>(handle));
+#else
+  return dlclose(handle);
+#endif
+}
+
+FUNC_POINTER GetSymbol(void* handle, const char* symbol) {
+#ifdef _WIN32
+  return GetProcAddress(static_cast<HMODULE>(handle), symbol);
+#else
+  return dlsym(handle, symbol);
+#endif
+}
+
+ERROR_TYPE GetError() {
+#ifdef _WIN32
+  return GetLastError();
+#else
+  return dlerror();
+#endif
+}
+
 }  // namespace
 
 struct JniInvocationImpl final {
@@ -81,7 +135,7 @@ struct JniInvocationImpl final {
   JniInvocationImpl(const JniInvocationImpl&) = delete;
   JniInvocationImpl& operator=(const JniInvocationImpl&) = delete;
 
-  bool FindSymbol(void** pointer, const char* symbol);
+  bool FindSymbol(FUNC_POINTER* pointer, const char* symbol);
 
   static JniInvocationImpl* jni_invocation_;
 
@@ -113,7 +167,7 @@ JniInvocationImpl::JniInvocationImpl() :
 JniInvocationImpl::~JniInvocationImpl() {
   jni_invocation_ = NULL;
   if (handle_ != NULL) {
-    dlclose(handle_);
+    CloseLibrary(handle_);
   }
 }
 
@@ -168,16 +222,11 @@ bool JniInvocationImpl::Init(const char* library) {
   char* buffer = NULL;
 #endif
   library = GetLibrary(library, buffer);
-  // Load with RTLD_NODELETE in order to ensure that libart.so is not unmapped when it is closed.
-  // This is due to the fact that it is possible that some threads might have yet to finish
-  // exiting even after JNI_DeleteJavaVM returns, which can lead to segfaults if the library is
-  // unloaded.
-  const int kDlopenFlags = RTLD_NOW | RTLD_NODELETE;
-  handle_ = dlopen(library, kDlopenFlags);
+  handle_ = OpenLibrary(library);
   if (handle_ == NULL) {
     if (strcmp(library, kLibraryFallback) == 0) {
       // Nothing else to try.
-      ALOGE("Failed to dlopen %s: %s", library, dlerror());
+      ALOGE("Failed to dlopen %s: " STRINGIFY(ERROR_FORMAT_TYPE), library, GetError());
       return false;
     }
     // Note that this is enough to get something like the zygote
@@ -185,24 +234,24 @@ bool JniInvocationImpl::Init(const char* library) {
     // because we are root and not the system user. See
     // RuntimeInit.commonInit for where we fix up the property to
     // avoid future fallbacks. http://b/11463182
-    ALOGW("Falling back from %s to %s after dlopen error: %s",
-          library, kLibraryFallback, dlerror());
+    ALOGW("Falling back from %s to %s after dlopen error: " STRINGIFY(ERROR_FORMAT_TYPE),
+          library, kLibraryFallback, GetError());
     library = kLibraryFallback;
-    handle_ = dlopen(library, kDlopenFlags);
+    handle_ = OpenLibrary(library);
     if (handle_ == NULL) {
-      ALOGE("Failed to dlopen %s: %s", library, dlerror());
+      ALOGE("Failed to dlopen %s: " STRINGIFY(ERROR_FORMAT_TYPE), library, GetError());
       return false;
     }
   }
-  if (!FindSymbol(reinterpret_cast<void**>(&JNI_GetDefaultJavaVMInitArgs_),
+  if (!FindSymbol(reinterpret_cast<FUNC_POINTER*>(&JNI_GetDefaultJavaVMInitArgs_),
                   "JNI_GetDefaultJavaVMInitArgs")) {
     return false;
   }
-  if (!FindSymbol(reinterpret_cast<void**>(&JNI_CreateJavaVM_),
+  if (!FindSymbol(reinterpret_cast<FUNC_POINTER*>(&JNI_CreateJavaVM_),
                   "JNI_CreateJavaVM")) {
     return false;
   }
-  if (!FindSymbol(reinterpret_cast<void**>(&JNI_GetCreatedJavaVMs_),
+  if (!FindSymbol(reinterpret_cast<FUNC_POINTER*>(&JNI_GetCreatedJavaVMs_),
                   "JNI_GetCreatedJavaVMs")) {
     return false;
   }
@@ -221,11 +270,11 @@ jint JniInvocationImpl::JNI_GetCreatedJavaVMs(JavaVM** vms, jsize size, jsize* v
   return JNI_GetCreatedJavaVMs_(vms, size, vm_count);
 }
 
-bool JniInvocationImpl::FindSymbol(void** pointer, const char* symbol) {
-  *pointer = dlsym(handle_, symbol);
+bool JniInvocationImpl::FindSymbol(FUNC_POINTER* pointer, const char* symbol) {
+  *pointer = GetSymbol(handle_, symbol);
   if (*pointer == NULL) {
-    ALOGE("Failed to find symbol %s: %s\n", symbol, dlerror());
-    dlclose(handle_);
+    ALOGE("Failed to find symbol %s: " STRINGIFY(ERROR_FORMAT_TYPE) "\n", symbol, GetError());
+    CloseLibrary(handle_);
     handle_ = NULL;
     return false;
   }
